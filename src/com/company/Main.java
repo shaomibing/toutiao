f0e72b77a961bba95346589ac5b6231593a3d8fc
xuhaiyan <haiyan.xu.vip@gmail.com>
2017-07-03 15:49:07 +0800

    hello 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("HelloWorld");
+        System.out.println("HelloWorld2");
     }
 }
